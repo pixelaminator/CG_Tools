21aa115b7b588e560e753811450e5c0722a2c6fb
afansyah <afansyah.dg@gmail.com>
2017-09-06 14:30:27 +0700

    Change for VS2017, add CorelDRAW 2017 export

--- CG_FileManagement/CG_PublishDigital/SavingTest.vb
+++ CG_FileManagement/CG_PublishDigital/SavingTest.vb
@@ -1,36 +1,37 @@
 ﻿Imports vgApp = Corel.Interop.VGCore
 Imports Doc = Corel.Interop.VGCore.Document
-    Public Class SavingTest
-        'Public WithEvents appDraw As Corel.Interop.VGCore.Application
-        Dim filename As String
-        'Dim stat As New vgApp.AppStatus
-        Dim WithEvents cdraw As Corel.Interop.VGCore.Application = NewCDRApp.appDraw
+Imports Corel.Interop.VGCore
 
-        Public Sub New()
-            InitializeComponent()
-            filename = "\\10.10.0.10\graha\Setting\TEST.cdr"
-            bWorker.WorkerSupportsCancellation = True
-            bWorker.WorkerReportsProgress = True
-        End Sub
+Public Class SavingTest
+    'Public WithEvents appDraw As Corel.Interop.VGCore.Application
+    Dim filename As String
+    'Dim stat As New vgApp.AppStatus
+    Dim WithEvents cdraw As Corel.Interop.VGCore.Application = NewCDRApp.appDraw
 
-        Private Sub bt_save_Click(sender As Object, e As System.EventArgs) Handles bt_save.Click
-            If bWorker.IsBusy = False Then
-                bWorker.RunWorkerAsync()
-            End If
-        End Sub
+    Public Sub New()
+        InitializeComponent()
+        filename = "\\10.10.0.10\graha\Setting\TEST.cdr"
+        bWorker.WorkerSupportsCancellation = True
+        bWorker.WorkerReportsProgress = True
+    End Sub
 
-        Private Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
-            If bWorker.IsBusy = True Then
-                bWorker.CancelAsync()
-            End If
-        End Sub
+    Private Sub bt_save_Click(sender As Object, e As System.EventArgs) Handles bt_save.Click
+        If bWorker.IsBusy = False Then
+            bWorker.RunWorkerAsync()
+        End If
+    End Sub
 
-        Private Sub bWorker_DoWork() Handles bWorker.DoWork
-            cdraw.ActiveDocument.Save()
-            'bWorker.ReportProgress(stat.Progress)
-        End Sub
-        Private Sub bWorker_ProgressChanged() Handles bWorker.ProgressChanged
-            ProgressBar1.Value += 100
-        End Sub
+    Private Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
+        If bWorker.IsBusy = True Then
+            bWorker.CancelAsync()
+        End If
+    End Sub
 
-    End Class
+    Private Sub bWorker_DoWork() Handles bWorker.DoWork
+        cdraw.ActiveDocument.SaveAs(filename)
+        'bWorker.ReportProgress(stat.Progress)
+    End Sub
+    Private Sub bWorker_ProgressChanged() Handles bWorker.ProgressChanged
+        ProgressBar1.Value += 100
+    End Sub
+End Class
