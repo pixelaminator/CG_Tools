b39666ac9b5a3a882488de03bfabeaab94757bd3
afansyah <afansyah.dg@gmail.com>
2017-10-14 16:09:06 +0700

    Revert "Setting Tools Works"
    This reverts commit 84f7da0399d59662f7dfc0132c214664a4bb7f55.

--- CG_SettingTools/CG_SpellChecker/MainForm.vb
+++ CG_SettingTools/CG_SpellChecker/MainForm.vb
@@ -1,31 +1,52 @@
-﻿Imports System.Collections
-Imports Corel.Interop.VGCore
+﻿Imports System
+Imports System.Reflection
+Imports System.Windows.Forms
 Imports i00SpellCheck
 
 Public Class MainForm
-    Private Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
-        'Dim iSpellCheckDialog = TryCast(TextBox1.SpellCheck, i00SpellCheck.SpellCheckControlBase.iSpellCheckDialog)
-        'If iSpellCheckDialog IsNot Nothing Then
-        '    iSpellCheckDialog.ShowDialog()
-        'End If
-        CollectTexts()
+    Dim AssemblyLoadError As Boolean
+    Private Sub MainForm_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+        'Loads 3rd party assembly from addon folder
+        AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf AssemblyLoadHandler
     End Sub
 
-    Private Sub CollectTexts()
-        Dim p As Page, s As Shape, sr As ShapeRange
-        Dim i As Integer
-        Dim ht As New Hashtable
+    Function AssemblyLoadHandler(ByVal sender As Object,
+                       ByVal args As ResolveEventArgs) As [Assembly]
+        'This handler is called only when the common language runtime tries to bind to the assembly and fails.        
 
-        i = cdraw.ActivePage.Index
-        For Each p In cdraw.ActiveDocument.Pages
-            p.Activate()
-            sr = cdraw.ActivePage.Shapes.FindShapes(Query:="@type = 'text:artistic' or @type = 'text:paragraph'")
-            For Each s In sr
-                If ht.ContainsKey(s.StaticID) = False Then
-                    ht.Add(s.StaticID, s.Text.Story.Text)
-                End If
-            Next s
-        Next p
-        cdraw.ActiveDocument.Pages(i).Activate()
+        'Retrieve the list of referenced assemblies in an array of AssemblyName.
+        Dim objExecutingAssemblies As [Assembly]
+        objExecutingAssemblies = [Assembly].GetExecutingAssembly()
+        Dim arrReferencedAssmbNames() As AssemblyName
+        arrReferencedAssmbNames = objExecutingAssemblies.GetReferencedAssemblies()
+
+        'Loop through the array of referenced assembly names.
+        Dim strAssmbName As AssemblyName
+        For Each strAssmbName In arrReferencedAssmbNames
+            Dim MyAssembly As [Assembly]
+            'Look for the assembly names that have raised the "AssemblyResolve" event.
+            If (strAssmbName.FullName.Substring(0, strAssmbName.FullName.IndexOf(",")) = args.Name.Substring(0, args.Name.IndexOf(","))) Then
+
+                'Build the path of the assembly from where it has to be loaded.
+                Dim strTempAssmbPath As String
+                strTempAssmbPath = Application.StartupPath + "\Addons\CG_Tools\" & args.Name.Substring(0, args.Name.IndexOf(",")) & ".dll"
+
+                'Load the assembly from the specified path. 
+                Try
+                    MyAssembly = [Assembly].LoadFrom(strTempAssmbPath)
+                    'Return the loaded assembly.
+                    Return MyAssembly
+                Catch ex As Exception
+                    AssemblyLoadError = True
+                    Exit Function
+                End Try
+            End If
+        Next
+    End Function
+    Private Sub Button1_Click(sender As Object, e As System.EventArgs) Handles Button1.Click
+        Dim iSpellCheckDialog = TryCast(TextBox1.SpellCheck, i00SpellCheck.SpellCheckControlBase.iSpellCheckDialog)
+        If iSpellCheckDialog IsNot Nothing Then
+            iSpellCheckDialog.ShowDialog()
+        End If
     End Sub
 End Class
--- CG_SettingTools/Main.vb
+++ CG_SettingTools/Main.vb
@@ -1,53 +1,9 @@
 ﻿Imports RGiesecke.DllExport
-Imports System.Reflection
-Imports System.Windows.Forms
-Imports System
 
 Public Module Main
-    Public AssemblyLoadError As Boolean
-
-    'Sub New()
-    '    'Loads 3rd party assembly from addon folder
-    '    AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf AssemblyLoadHandler
-    'End Sub
-
     <DllExport("CGSpellCheck")>
     Public Sub CG_SpellCheck()
         Dim frm As New MainForm
         frm.ShowDialog()
     End Sub
-
-    'Function AssemblyLoadHandler(ByVal sender As Object,
-    '               ByVal args As ResolveEventArgs) As [Assembly]
-    '    'This handler is called only when the common language runtime tries to bind to the assembly and fails.        
-
-    '    'Retrieve the list of referenced assemblies in an array of AssemblyName.
-    '    Dim objExecutingAssemblies As [Assembly]
-    '    objExecutingAssemblies = [Assembly].GetExecutingAssembly()
-    '    Dim arrReferencedAssmbNames() As AssemblyName
-    '    arrReferencedAssmbNames = objExecutingAssemblies.GetReferencedAssemblies()
-
-    '    'Loop through the array of referenced assembly names.
-    '    Dim strAssmbName As AssemblyName
-    '    For Each strAssmbName In arrReferencedAssmbNames
-    '        Dim MyAssembly As [Assembly]
-    '        'Look for the assembly names that have raised the "AssemblyResolve" event.
-    '        If (strAssmbName.FullName.Substring(0, strAssmbName.FullName.IndexOf(",")) = args.Name.Substring(0, args.Name.IndexOf(","))) Then
-
-    '            'Build the path of the assembly from where it has to be loaded.
-    '            Dim strTempAssmbPath As String
-    '            strTempAssmbPath = Application.StartupPath + "\Addons\CG_Tools\" & args.Name.Substring(0, args.Name.IndexOf(",")) & ".dll"
-
-    '            'Load the assembly from the specified path. 
-    '            Try
-    '                MyAssembly = [Assembly].LoadFrom(strTempAssmbPath)
-    '                'Return the loaded assembly.
-    '                Return MyAssembly
-    '            Catch ex As Exception
-    '                AssemblyLoadError = True
-    '                Exit Function
-    '            End Try
-    '        End If
-    '    Next
-    'End Function
 End Module
