c1a90d43ac782043b921bbb2bd214eb38b01ba04
afansyah <afansyah.dg@gmail.com>
2017-10-14 16:05:10 +0700

    Revert "Revert "Setting Tools Works""
    This reverts commit 3d9c418dad9bce234fe642b86eb005c4fa04b337.

---
+++ CG_SettingTools/NewCDRApp.vb
@@ -0,0 +1,19 @@
+﻿Imports Corel.Interop.VGCore
+Imports Microsoft.VisualBasic.Interaction
+Public Module CDRApp
+    Public WithEvents cdraw As Corel.Interop.VGCore.Application
+    'Public Sub New(app As Object)
+    '    appDraw = CType(app, Corel.Interop.VGCore.Application)
+    'End Sub
+    Sub New()
+        If cdraw Is Nothing Then
+#If X7 Then
+            cdraw = CType(CreateObject("CorelDRAW.Application.17"), Application)
+#ElseIf X8 Then
+            cdraw = CType(CreateObject("CorelDRAW.Application.18"), Application)
+#ElseIf X9 Then
+            cdraw = CType(CreateObject("CorelDRAW.Application.19"), Application)
+#End If
+        End If
+    End Sub
+End Module
