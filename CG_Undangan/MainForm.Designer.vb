932b4541c1add02a78d6e27b74bc2b5d9b10975d
Afansyah <afansyah.dg@gmail.com>
2018-03-15 16:24:35 +0700

    Form design work and fix assembly path

--- CG_Undangan/MainForm.Designer.vb
+++ CG_Undangan/MainForm.Designer.vb
@@ -22,16 +22,262 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.KryptonSplitContainer1 = New ComponentFactory.Krypton.Toolkit.KryptonSplitContainer()
+        Me.KryptonManager1 = New ComponentFactory.Krypton.Toolkit.KryptonManager(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KryptonHeaderGroup1 = New ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup()
+        Me.KryptonButton1 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.ButtonSpecHeaderGroup1 = New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup()
+        Me.KryptonHeaderGroup3 = New ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup()
+        Me.KryptonPanel1 = New ComponentFactory.Krypton.Toolkit.KryptonPanel()
+        Me.KryptonHeaderGroup2 = New ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup()
+        Me.ButtonSpecHeaderGroup2 = New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup()
+        Me.KryptonButton2 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        Me.KryptonHeaderGroup4 = New ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup()
+        Me.ButtonSpecHeaderGroup3 = New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup()
+        Me.KryptonButton3 = New ComponentFactory.Krypton.Toolkit.KryptonButton()
+        CType(Me.KryptonSplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonSplitContainer1.Panel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonSplitContainer1.Panel1.SuspendLayout()
+        CType(Me.KryptonSplitContainer1.Panel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonSplitContainer1.Panel2.SuspendLayout()
+        Me.KryptonSplitContainer1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.KryptonHeaderGroup1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonHeaderGroup1.Panel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonHeaderGroup1.Panel.SuspendLayout()
+        Me.KryptonHeaderGroup1.SuspendLayout()
+        CType(Me.KryptonHeaderGroup3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonHeaderGroup3.Panel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonHeaderGroup3.Panel.SuspendLayout()
+        Me.KryptonHeaderGroup3.SuspendLayout()
+        CType(Me.KryptonPanel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonHeaderGroup2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonHeaderGroup2.Panel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonHeaderGroup2.Panel.SuspendLayout()
+        Me.KryptonHeaderGroup2.SuspendLayout()
+        CType(Me.KryptonHeaderGroup4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KryptonHeaderGroup4.Panel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.KryptonHeaderGroup4.Panel.SuspendLayout()
+        Me.KryptonHeaderGroup4.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'KryptonSplitContainer1
+        '
+        Me.KryptonSplitContainer1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.KryptonSplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KryptonSplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.KryptonSplitContainer1.IsSplitterFixed = True
+        Me.KryptonSplitContainer1.Location = New System.Drawing.Point(0, 24)
+        Me.KryptonSplitContainer1.Name = "KryptonSplitContainer1"
+        '
+        'KryptonSplitContainer1.Panel1
+        '
+        Me.KryptonSplitContainer1.Panel1.Controls.Add(Me.KryptonHeaderGroup4)
+        Me.KryptonSplitContainer1.Panel1.Controls.Add(Me.KryptonHeaderGroup2)
+        Me.KryptonSplitContainer1.Panel1.Controls.Add(Me.KryptonHeaderGroup1)
+        '
+        'KryptonSplitContainer1.Panel2
+        '
+        Me.KryptonSplitContainer1.Panel2.Controls.Add(Me.KryptonHeaderGroup3)
+        Me.KryptonSplitContainer1.Size = New System.Drawing.Size(694, 540)
+        Me.KryptonSplitContainer1.SplitterDistance = 197
+        Me.KryptonSplitContainer1.TabIndex = 1
+        '
+        'KryptonManager1
+        '
+        Me.KryptonManager1.GlobalPaletteMode = ComponentFactory.Krypton.Toolkit.PaletteModeManager.Office2010Silver
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(694, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'KryptonHeaderGroup1
+        '
+        Me.KryptonHeaderGroup1.AutoSize = True
+        Me.KryptonHeaderGroup1.ButtonSpecs.AddRange(New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup() {Me.ButtonSpecHeaderGroup1})
+        Me.KryptonHeaderGroup1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonHeaderGroup1.GroupBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.PanelAlternate
+        Me.KryptonHeaderGroup1.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.DockInactive
+        Me.KryptonHeaderGroup1.HeaderVisibleSecondary = False
+        Me.KryptonHeaderGroup1.Location = New System.Drawing.Point(0, 0)
+        Me.KryptonHeaderGroup1.Name = "KryptonHeaderGroup1"
+        '
+        'KryptonHeaderGroup1.Panel
+        '
+        Me.KryptonHeaderGroup1.Panel.Controls.Add(Me.KryptonButton1)
+        Me.KryptonHeaderGroup1.Panel.Padding = New System.Windows.Forms.Padding(5)
+        Me.KryptonHeaderGroup1.Size = New System.Drawing.Size(197, 72)
+        Me.KryptonHeaderGroup1.TabIndex = 0
+        Me.KryptonHeaderGroup1.ValuesPrimary.Image = Nothing
+        '
+        'KryptonButton1
+        '
+        Me.KryptonButton1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonButton1.Location = New System.Drawing.Point(5, 5)
+        Me.KryptonButton1.Name = "KryptonButton1"
+        Me.KryptonButton1.Size = New System.Drawing.Size(185, 39)
+        Me.KryptonButton1.TabIndex = 0
+        Me.KryptonButton1.Values.Text = "KryptonButton1"
+        '
+        'ButtonSpecHeaderGroup1
+        '
+        Me.ButtonSpecHeaderGroup1.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.ArrowUp
+        Me.ButtonSpecHeaderGroup1.UniqueName = "F089F9214EF9420B1A825086AB681999"
+        '
+        'KryptonHeaderGroup3
+        '
+        Me.KryptonHeaderGroup3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KryptonHeaderGroup3.Location = New System.Drawing.Point(0, 0)
+        Me.KryptonHeaderGroup3.Name = "KryptonHeaderGroup3"
+        '
+        'KryptonHeaderGroup3.Panel
+        '
+        Me.KryptonHeaderGroup3.Panel.Controls.Add(Me.KryptonPanel1)
+        Me.KryptonHeaderGroup3.Size = New System.Drawing.Size(492, 540)
+        Me.KryptonHeaderGroup3.StateCommon.HeaderPrimary.Content.Image.ImageH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Far
+        Me.KryptonHeaderGroup3.TabIndex = 0
+        '
+        'KryptonPanel1
+        '
+        Me.KryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.KryptonPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.KryptonPanel1.Name = "KryptonPanel1"
+        Me.KryptonPanel1.Size = New System.Drawing.Size(490, 487)
+        Me.KryptonPanel1.TabIndex = 0
+        '
+        'KryptonHeaderGroup2
+        '
+        Me.KryptonHeaderGroup2.AutoSize = True
+        Me.KryptonHeaderGroup2.ButtonSpecs.AddRange(New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup() {Me.ButtonSpecHeaderGroup2})
+        Me.KryptonHeaderGroup2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonHeaderGroup2.GroupBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.PanelAlternate
+        Me.KryptonHeaderGroup2.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.DockInactive
+        Me.KryptonHeaderGroup2.HeaderVisibleSecondary = False
+        Me.KryptonHeaderGroup2.Location = New System.Drawing.Point(0, 72)
+        Me.KryptonHeaderGroup2.Name = "KryptonHeaderGroup2"
+        '
+        'KryptonHeaderGroup2.Panel
+        '
+        Me.KryptonHeaderGroup2.Panel.Controls.Add(Me.KryptonButton2)
+        Me.KryptonHeaderGroup2.Panel.Padding = New System.Windows.Forms.Padding(5)
+        Me.KryptonHeaderGroup2.Size = New System.Drawing.Size(197, 72)
+        Me.KryptonHeaderGroup2.TabIndex = 1
+        Me.KryptonHeaderGroup2.ValuesPrimary.Image = Nothing
+        '
+        'ButtonSpecHeaderGroup2
+        '
+        Me.ButtonSpecHeaderGroup2.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.ArrowUp
+        Me.ButtonSpecHeaderGroup2.UniqueName = "F089F9214EF9420B1A825086AB681999"
+        '
+        'KryptonButton2
+        '
+        Me.KryptonButton2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonButton2.Location = New System.Drawing.Point(5, 5)
+        Me.KryptonButton2.Name = "KryptonButton2"
+        Me.KryptonButton2.Size = New System.Drawing.Size(185, 39)
+        Me.KryptonButton2.TabIndex = 0
+        Me.KryptonButton2.Values.Text = "KryptonButton2"
+        '
+        'KryptonHeaderGroup4
+        '
+        Me.KryptonHeaderGroup4.AutoSize = True
+        Me.KryptonHeaderGroup4.ButtonSpecs.AddRange(New ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup() {Me.ButtonSpecHeaderGroup3})
+        Me.KryptonHeaderGroup4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonHeaderGroup4.GroupBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.PanelAlternate
+        Me.KryptonHeaderGroup4.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.DockInactive
+        Me.KryptonHeaderGroup4.HeaderVisibleSecondary = False
+        Me.KryptonHeaderGroup4.Location = New System.Drawing.Point(0, 144)
+        Me.KryptonHeaderGroup4.Name = "KryptonHeaderGroup4"
+        '
+        'KryptonHeaderGroup4.Panel
+        '
+        Me.KryptonHeaderGroup4.Panel.Controls.Add(Me.KryptonButton3)
+        Me.KryptonHeaderGroup4.Panel.Padding = New System.Windows.Forms.Padding(5)
+        Me.KryptonHeaderGroup4.Size = New System.Drawing.Size(197, 72)
+        Me.KryptonHeaderGroup4.TabIndex = 2
+        Me.KryptonHeaderGroup4.ValuesPrimary.Image = Nothing
+        '
+        'ButtonSpecHeaderGroup3
+        '
+        Me.ButtonSpecHeaderGroup3.Type = ComponentFactory.Krypton.Toolkit.PaletteButtonSpecStyle.ArrowUp
+        Me.ButtonSpecHeaderGroup3.UniqueName = "F089F9214EF9420B1A825086AB681999"
+        '
+        'KryptonButton3
+        '
+        Me.KryptonButton3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.KryptonButton3.Location = New System.Drawing.Point(5, 5)
+        Me.KryptonButton3.Name = "KryptonButton3"
+        Me.KryptonButton3.Size = New System.Drawing.Size(185, 39)
+        Me.KryptonButton3.TabIndex = 0
+        Me.KryptonButton3.Values.Text = "KryptonButton3"
         '
         'MainForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(694, 437)
+        Me.ClientSize = New System.Drawing.Size(694, 564)
+        Me.Controls.Add(Me.KryptonSplitContainer1)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Name = "MainForm"
         Me.Text = "MainForm"
+        CType(Me.KryptonSplitContainer1.Panel1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonSplitContainer1.Panel1.ResumeLayout(False)
+        Me.KryptonSplitContainer1.Panel1.PerformLayout()
+        CType(Me.KryptonSplitContainer1.Panel2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonSplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.KryptonSplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonSplitContainer1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.KryptonHeaderGroup1.Panel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup1.Panel.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup1.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup3.Panel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup3.Panel.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup3.ResumeLayout(False)
+        CType(Me.KryptonPanel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KryptonHeaderGroup2.Panel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup2.Panel.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup2.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup4.Panel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup4.Panel.ResumeLayout(False)
+        CType(Me.KryptonHeaderGroup4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.KryptonHeaderGroup4.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents KryptonSplitContainer1 As ComponentFactory.Krypton.Toolkit.KryptonSplitContainer
+    Friend WithEvents KryptonManager1 As ComponentFactory.Krypton.Toolkit.KryptonManager
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents KryptonHeaderGroup1 As ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup
+    Friend WithEvents ButtonSpecHeaderGroup1 As ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup
+    Friend WithEvents KryptonButton1 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonHeaderGroup3 As ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup
+    Friend WithEvents KryptonPanel1 As ComponentFactory.Krypton.Toolkit.KryptonPanel
+    Friend WithEvents KryptonHeaderGroup4 As ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup
+    Friend WithEvents ButtonSpecHeaderGroup3 As ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup
+    Friend WithEvents KryptonButton3 As ComponentFactory.Krypton.Toolkit.KryptonButton
+    Friend WithEvents KryptonHeaderGroup2 As ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup
+    Friend WithEvents ButtonSpecHeaderGroup2 As ComponentFactory.Krypton.Toolkit.ButtonSpecHeaderGroup
+    Friend WithEvents KryptonButton2 As ComponentFactory.Krypton.Toolkit.KryptonButton
 End Class
